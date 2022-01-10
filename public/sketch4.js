7d7c27b912456ebaeac755c12de276f4dfe7b387
giampoborm <90754684+giampoborm@users.noreply.github.com>
2022-01-10 11:14:40 +0100

    updating

---
+++ public/sketch4.js
@@ -0,0 +1,115 @@
+let clientSocket = io();
+
+clientSocket.on("connect", newConnection)
+clientSocket.on("mouseBroadcast", newBroadcast);
+
+function newConnection(){
+  console.log(clientSocket.id);
+}
+
+const urlString = window.location.href;
+let url = new URL(urlString);
+console.log(urlString);
+
+let myButton;
+let myButton1;
+
+let lines = [];
+
+let penColor;
+let penThickness;
+
+let myImage;
+let sfondo;
+
+function preload() {
+  myImage = loadImage("./assets/chattando.png");
+  sfondo = loadImage("./assets/sfondo.jpg")
+}
+
+
+//running newConnection function when a new connection is made
+clientSocket.on("connect", newConnection);
+
+//defining the newConnection function
+function newConnection() {
+  //printing the connections' ids
+ console.log(clientSocket.id);
+}
+
+//running newBroadcast function when receiving "mouse" message
+clientSocket.on("mouseBroadcast", newBroadcast);
+
+//defining newBroadcast function
+function newBroadcast(data) {
+  console.log(data);
+  drawLine(
+    data.px * width,
+    data.py * height,
+    data.x * width,
+    data.y * height,
+    data.color,
+    data.thickness
+  );
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(sfondo);
+
+  let container =  createDiv().style("position: absolute").position(width-100, height-100);
+  myButton1 = createElement("button", "previous").parent(container);
+  myButton1.mouseClicked(function () {
+    window.open("http://localhost:3000/third-page.html", "_self");
+  });
+  myButton = createElement("button", "next meme").parent(container);
+  myButton.mouseClicked(function () {
+    window.open("http://localhost:3000/fifth-page.html", "_self");
+  });
+ 
+
+  imageMode(CENTER)
+  image(myImage, width/2, height/2, myImage.width, myImage.height);
+
+  let options = createDiv().style("position: absolute").position(10, height-100).style("z-index:1");
+  penColor = createColorPicker("#000000").parent(options);
+  penThickness = createSelect(false)
+    .parent(options)
+  penThickness.option("1");
+  penThickness.option("3");
+  penThickness.option("5");
+  penThickness.option("7");
+  penThickness.selected("1");
+}
+
+function draw() {}
+
+//sending to the server every mouse position
+function mouseDragged() {
+  //type of message to send
+  let message = {
+    x: mouseX / width,
+    y: mouseY / height,
+    px: pmouseX / width,
+    py: pmouseY / height,
+    color: penColor.value(),
+    thickness: penThickness.value(),
+  };
+
+  //sending the message, from client to server
+  clientSocket.emit("mouse", message);
+  drawLine(
+    pmouseX,
+    pmouseY,
+    mouseX,
+    mouseY,
+    penColor.value(),
+    penThickness.value()
+  );
+}
+
+function drawLine(px, py, x, y, color, thickness) {
+  stroke(color);
+  strokeWeight(thickness);
+  line(px, py, x, y);
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+console.log("aooooooo");
+
+let express = require("express");
+
+let app = express();
+
+let port = 3000;
+
+let server = app.listen(port)
+
+console.log("server is running on http://localhost:"+ port);
+
+app.use(express.static("public"));
+
+let serverSocket = require("socket.io");
+
+let io = serverSocket(server);
+
+io.on("connection", newConnection);
+
+function newConnection(newSocket) {
+    console.log(newSocket.id);
+
+    newSocket.on("mouse", mouseMessage);
+
+    function mouseMessage(dataReceived){
+        console.log(dataReceived);
+        newSocket.broadcast.emit("mouseBroadcast", dataReceived);
+    } 
+}
